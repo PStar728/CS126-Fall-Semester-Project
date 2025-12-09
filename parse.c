ac8fe0de6f44b335b6800855cb8a43f448414eca
Pstar728 <frankiegonzalez628@icloud.com>
2025-12-09 14:31:28 -0600

    Updates to parse.c

--- parse.c
+++ parse.c
@@ -1,11 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 #include "parse.h"
 
 char* read_line(){
     char* line = NULL;
     size_t size = 0;
 
-    
+    ssize_t len = getline(&line, &size, stdin);
+
+    if (len == -1) {
+        free(line);
+        return NULL;
+        perror("get line in parse");
+    }
+    return line;
 }
 char** parse_line(char* line){
-    // Function implementation goes here
+    
 }
